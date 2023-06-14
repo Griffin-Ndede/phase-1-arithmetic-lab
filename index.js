50b504945a20c9df7981899f6b0f716569421b7c
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-06-14 17:00:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+console.log (multiply);
+
+let random = Math.ceil(Math.random()*100);
+
+let num3 = 16;
+let num4 = 6;
+
+let mod = num3 % num4;
+console.log (mod);
+
+let set = ( 2, 4, 8, 10, 20 );
+let max =  Math.max (2, 4, 8, 10, 20 )
